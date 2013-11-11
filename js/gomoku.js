53cf7cc50bf9fbbc4b61d95962ec607d54de9458
Hans H Chou <hchou@fas.harvard.edu>
2013-11-11 15:58:55 -0500

    Tweaking winner determination logic.

--- js/gomoku.js
+++ js/gomoku.js
@@ -1,32 +1,39 @@
 // Arrays to store X,Y positions of Black and White stones
-var whiteX = new Array();
-var whiteY = new Array();
-var blackX = new Array();
-var blackY = new Array();
+var whiteX2Y = new Array;
+for (var i = 0; i <= 11; i++) {
+    whiteX2Y[i] = [];
+}
+var blackX2Y = new Array;
+for (var i = 0; i <= 11; i++) {
+    blackX2Y[i] = [];
+}
+var whiteDiv = '<div class="stone-white"></div>';
+var blackDiv = '<div class="stone-black"></div>';
 
 $('.grid').click(function() {
 
     // If the box isn't already occupied by a white or black stone then proceed
-    if ($(this).html() != '<div class="stone-white"></div>' && $(this).html() != '<div class="stone-black"></div>') {
+    if ($(this).html() != whiteDiv && $(this).html() != blackDiv) {
         var position = $(this).position();
         var x = Math.round(position.left / 50 + 1);
         var y = Math.round(position.top / 50 + 1);
-        console.log('turn: ', turnCnt);
+        console.log('turn: ' + (turnCnt + 1));
         console.log('x: ' + x);
         console.log('y: ' + y);
         // If modulus of turnCnt (turn count) is = that means it's black's turn
         if (turnCnt % 2 == 0) {
-            $(this).append("<div class='stone-black'></div>");
-            whiteX.push(x);
-            whiteY.push(y);
-            // Check if black's won
-            checkVertical(x, y, 'black');
+            $(this).append(blackDiv);
+            // Storing black stone positions in array
+            blackX2Y[x][y] = 1;
+            // Check vertical win for Black player
+            checkWinner(x, y, blackX2Y);
         } else {
-            $(this).append("<div class='stone-white'></div>");
-            blackX.push(x);
-            blackY.push(y);
-            // Check if white's won
-            checkVertical(x, y, 'white');
+            $(this).append(whiteDiv);
+            // Storing white stone positions in array
+            whiteX2Y[x][y] = 1;
+            console.log('whiteX2Y[x][y]: ' + whiteX2Y[x][y]);
+            // Check vertical win for White player
+            checkWinner(x, y, whiteX2Y);
         }
         turnCnt++;
 
@@ -35,14 +42,27 @@ $('.grid').click(function() {
 
 });
 
-function checkVertical(x, y, color) {
-
+function checkWinner(x, y, X2Y) {
+    var adjacent_count = 0;
     for (var i = y + 1; i < y + 5; i++) {
         if (i > 11) {
             console.log('out of bound');
             break;
         }
-        console.log(color + ': checking x=' + x + ' y=' + i);
+        //console.log('checking x=' + x + ' y=' + i);
+        //console.log(X2Y[x][i]);
+        //if (typeof X2Y[x] != 'undefined' && X2Y[x] == i) {
+        //if (typeof X2Y[x][i] != 'undefined') {
+            //console.log('X2Y[' + x + '] = ' + X2Y[x]);
+            if (X2Y[x][i] == 1) {
+                console.log('ADJACENT STONE (' + x + ',' + i +')');
+                adjacent_count++;
+            }
+            //console.log('adjacent vertical ' + color + ' stone');
+        //}
+        else {
+            break;
+        }
     }
 
     for (var i = y - 1; i > y - 5; i--) {
@@ -50,6 +70,23 @@ function checkVertical(x, y, color) {
             console.log('out of bound');
             break;
         }
-        console.log(color + ': checking x=' + x + ' y=' + i);
+        //console.log('checking x=' + x + ' y=' + i);
+        //console.log(X2Y[x][i]);
+        //if (typeof X2Y[x] != 'undefined' && X2Y[x] == i) {
+        //if (typeof X2Y[x][i] != 'undefined') {
+            //console.log('X2Y[' + x + '] = ' + X2Y[x]);
+            if (X2Y[x][i] == 1) {
+                console.log('ADJACENT STONE (' + x + ',' + i +')');
+                adjacent_count++;
+            }
+            //console.log('adjacent vertical ' + color + ' stone');
+        //}
+        else {
+            break;
+        }
+    }
+    
+    if (adjacent_count == 4) {
+        console.log('SOMEBODY WON, adjacent_count: ' + adjacent_count);
     }
 }
